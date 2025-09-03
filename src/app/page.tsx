6893f686f063e9b68ce10843220969c560beb23e
Eduardo Brito <eduardo370@outlook.pt>
2025-09-04 00:26:17 +0100

    mudança para o nextTS

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
